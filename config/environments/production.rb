1da568387994c5eddc14ac03c70e78895e32560b
Natalie Kueneman <tabalie@gmail.com>
2015-10-30 00:44:40 -0600

    Fix syntax again again again again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "http://frozen-earth-2819.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://frozen-earth-2819.herokuapp.com" }
 end
